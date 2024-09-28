677beb05b43415509b676c819ddec8fce257a548
bune <bune_assassin@hotmail.com>
2024-09-28 09:35:08 -0600

    Initial commit

---
+++ app/src/main/java/com/primerp/a6botones/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.primerp.a6botones;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // Aquí puedes manejar los datos que llegan desde los Intents explícitos
+    }
+}
